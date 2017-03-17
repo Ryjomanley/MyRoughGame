a926cf050c0ca16d914d57ca2111f426de74d77a
Ryan Manley <ryjomanley@yahoo.com>
2017-03-17 04:23:37 -0400

    First Commit

---
+++ Assets/MoveLeftRightUpDown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftRightUpDown : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//This moves left and right
+		transform.position += Vector3.right * Input.GetAxis ("Horizontal") * Time.deltaTime;
+		//This moves up and down
+		transform.position += Vector3.up * Input.GetAxis ("Vertical") * Time.deltaTime;
+
+	}
+}
